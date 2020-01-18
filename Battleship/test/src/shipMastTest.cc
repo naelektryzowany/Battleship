2909e24fc0e61ef98101e8a42158707e7630dd47
Łukasz Kamiński <lukasz@debian>
2020-01-18 22:59:39 +0100

    Unit Testy do klasy ShipMast

--- Battleship/test/src/shipMastTest.cc
+++ Battleship/test/src/shipMastTest.cc
@@ -3,18 +3,31 @@
 #include <iostream>
 
 class ShipMastTest : public ::testing::Test {
-protected:
-    virtual void SetUp() {
-        std::cout << "SetUp" << std::endl;
-    }
-    
-    virtual void TearDown() {
-        std::cout << "TearDown" << std::endl;
-    }
+  protected :
+    std::vector<int> mXYCoordinate{1,2};
+    Coordinates2d mCoordinatesForMast{mXYCoordinate};
+    ShipMast mShipMastToTest{mCoordinatesForMast};
 };
 
-TEST_F(ShipMastTest, SubtractTwoNumbers)
+TEST_F(ShipMastTest, checkInitialStatusTest)
 {
-    std::cout << "Test" << std::endl;
-    EXPECT_EQ(1,1);
+    bool expectedStatus = true;
+    bool actualStatus = mShipMastToTest.getStatus();
+    EXPECT_EQ(expectedStatus, actualStatus);
+}
+
+TEST_F(ShipMastTest, checkStatusAfterChangeTest)
+{
+    bool expectedStatus = false;
+    mShipMastToTest.setStatus(expectedStatus);
+    bool actualStatus = mShipMastToTest.getStatus();
+    EXPECT_EQ(expectedStatus, actualStatus);
+}
+
+TEST_F(ShipMastTest, checkIfProperCoordinatesAreReturnedTest)
+{
+    std::vector<int> expectedXYCoordinates = {1,2};
+    Coordinates2d expectedCoordinates(expectedXYCoordinates);
+    Coordinates2d actualCoordinates = mShipMastToTest.getCoordinates();
+    EXPECT_EQ(expectedCoordinates, actualCoordinates);
 }
