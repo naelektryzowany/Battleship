ca4e934c5552b628947c616ac3b317762c0bae35
Łukasz Kamiński <233098@edu.p.lodz.pl>
2020-01-19 17:55:22 +0100

    Unit testy dla klasy Ship2d

---
+++ Battleship/test/src/shipTest.cc
@@ -0,0 +1,58 @@
+#include "Ship2d.h"
+#include "gtest/gtest.h"
+#include <iostream>
+
+class ShipTest : public ::testing::Test {
+  protected :
+    std::vector<int> mCoordinateXYFirst{1,2};
+    std::vector<int> mCoordinateXYSecond{1,3};
+    Coordinates2d mCordinatesForMastFirst{mCoordinateXYFirst};
+    Coordinates2d mCordinatesForMastSecond{mCoordinateXYSecond};
+    std::vector<Coordinates2d> mShipCoordinates{mCoordinateXYFirst, mCoordinateXYSecond};
+    Ship2d mShipToTest{mShipCoordinates};
+};
+
+TEST_F(ShipTest, checkInitialStatusTest)
+{
+  bool expectedStatus = true;
+  bool actualStatus = mShipToTest.getStatus();
+  EXPECT_EQ(expectedStatus, actualStatus);
+}
+
+TEST_F(ShipTest, checkIfProperCoordinatesAreReturnedTest)
+{
+  std::vector<int> coordinateXYFirst{1,2};
+  std::vector<int> coordinateXYSecond{1,3};
+  Coordinates2d cordinatesForMastFirst{coordinateXYFirst};
+  Coordinates2d cordinatesForMastSecond{coordinateXYSecond};
+  std::vector<Coordinates2d> shipCoordinates{coordinateXYFirst, coordinateXYSecond};
+  Ship2d ship{shipCoordinates};
+  std::vector<std::reference_wrapper<const CoordinatesIf>> expectedCoordinates = ship.getCoordinates();
+  std::vector<std::reference_wrapper<const CoordinatesIf>> actualCoordinates = mShipToTest.getCoordinates();
+  bool expectedComparsionValue = true;
+  if(expectedCoordinates.size() != actualCoordinates.size()) {
+    expectedComparsionValue = false;
+  }
+  else {
+    for(int i = 0 ; i < expectedCoordinates.size() ; ++i) {
+      if(!(expectedCoordinates.at(i).get() == actualCoordinates.at(i).get())) {
+        expectedComparsionValue = false;
+        break;
+      }
+    }
+  }
+  EXPECT_TRUE(expectedComparsionValue);
+}
+
+TEST_F(ShipTest, checkIfShipIsDestroyedProperlyTest)
+{
+  std::vector<int> coordinateXYFirst{1,2};
+  std::vector<int> coordinateXYSecond{1,3};
+  Coordinates2d cordinatesToHitFirst{coordinateXYFirst};
+  Coordinates2d cordinatesToHitSecond{coordinateXYSecond};
+  mShipToTest.hit(cordinatesToHitFirst);
+  mShipToTest.hit(cordinatesToHitSecond);
+  bool expectedShipStatus = false;
+  bool actualShipStatus = mShipToTest.getStatus();
+  EXPECT_EQ(expectedShipStatus, actualShipStatus);
+}
